dbf2cf61da0dba3fafd7527ac4d421afe2615f36
georgiev098 <geor0014@hz.nl>
2023-05-16 17:30:27 +0200

    added path param to get method in main.py

--- app/main.py
+++ app/main.py
@@ -1,10 +1,18 @@
 # app/main.py
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Path
 
 app = FastAPI(title="FastAPI, Docker, and Traefik")
 
+inventory = {
+    1: {
+        'name': 'Milk',
+        'price': 4.25,
+        'brand': 'Clover',
+    }
+}
 
-@app.get("/")
-def read_root():
-    return {"hello": "world"}
+
+@app.get('/item/{item_id}')
+def get_item(item_id: int = Path(None, description='The ID of the item to get', gt=0)):
+    return inventory[item_id]
